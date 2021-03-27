8245af6c84913b3348a08ee92e64626c09f52a98
WincerChan <WincerChan@gmail.com>
2021-03-27 19:57:46 +0800

    Blog Construct

---
+++ deploy.fish
@@ -0,0 +1,5 @@
+git init                                                                                                                               
+git add .                                                                                                                              
+git commit -m "Blog Construct"                                                                                                         
+git remote add origin git@github.com:into-vyong/into-vyong.github.io.git                                                               
+git push origin master -f
